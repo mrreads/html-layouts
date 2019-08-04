22554c389d7680fd2be47577d3ceb0a6fa8d0c92
Alexander <mrreads@yandex.com>
2019-08-05 01:53:25 +0300

    hover added

--- site-1/js/main.js
+++ site-1/js/main.js
@@ -17,3 +17,18 @@ function clearProperties()
         document.querySelector(".block-one__nav__menu").style.display = "";
     }  
 }
+
+let items = document.querySelectorAll(".block-four__content__item__one");
+for (let i = 0; i < items.length; i++)
+{
+    items[i].addEventListener("mouseover", function()
+    {
+        items[i].style.boxShadow = "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)";
+        items[i].style.transform = "translateY(-10px)";
+    });
+    items[i].addEventListener("mouseout", function()
+    {
+        items[i].style.boxShadow = "";
+        items[i].style.transform = "";
+    });
+}
